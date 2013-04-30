7ddacce7971479a89bbaef015832a750bb065f99
Ayoub J. Idrissi <smartess@gmail.com>
2013-04-30 18:39:44 +0000

    test for model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,11 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+
+  email_regex = /[\w\-.]+@[a-z\d\-.]+\.[a-z]+/
+  validates :name, :presence => true,
+  				   :length => {:maximum => 30 }
+  validates :email, :presence => true,
+  				    :format => {:with => email_regex},
+  				    :uniqueness => {:case_sensitive => false}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,5 +12,42 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each)	do
+  	@attr = {:name => "Sample Name", :email => "sample@example.com"}
+  end
+
+  it "should create a valid user width valid attributes" do
+  	User.create!(@attr)
+  end
+
+  it "should require a name" do
+  	no_name_user = User.new(@attr.merge(:name => ""))
+  	no_name_user.should_not be_valid
+  end
+
+  it "should require an email" do
+  	no_email_user = User.new(@attr.merge(:email => ""))
+  	no_email_user.should_not be_valid
+  end
+
+  it "should reject names that are too long" do
+  		name_too_long = "a" * 50
+  		name_too_long_user = User.new(@attr.merge(:name => name_too_long))
+  		name_too_long_user.should_not be_valid
+  end
+
+  it "should reject invalid emails" do
+  	addresses = %w[test.com test@hostname,com test@test]
+  	addresses.each do |address|
+  		invalid_user_email = User.new(@attr.merge(:email => address))	
+  		invalid_user_email.should_not be_valid
+  	end
+  end
+  it "should reject double email addresses" do
+  	User.create!(@attr)
+  	user_with_duplicate_email = User.new(@attr)
+  	user_with_duplicate_email.should_not be_valid
+  end	
+
 end
